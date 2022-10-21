f1783f635dffd383af9f35e5b4018437581aaa42
ladyvee1 <veekayasuor@gmail.com>
2022-10-20 19:03:52 -0700

    jj

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,9 +7,15 @@
  *
  * Return: gives boolean
  */
+
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= '48' && c < '58')
+	{
 		return (1);
-	return (0);
+	}
+	else
+	{
+		return (0);
+	}
 }
