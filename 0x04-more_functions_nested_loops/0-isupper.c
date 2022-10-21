0426a373bb639ba54a2f84111d73c01fa2b38eed
ladyvee1 <veekayasuor@gmail.com>
2022-10-20 19:00:08 -0700

    hhh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,17 @@
  * _isupper - checks if uppercase
  * @c: character to check
  *
- * Return: gives 1 or 0
+ * Return: gives 1
+ * o otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '65' && c < '91')
 	{
 		return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
