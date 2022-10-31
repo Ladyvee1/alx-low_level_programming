156507a5e86257aef44fe9f4d5f4123522dfd9f9
ladyvee1 <veekayasuor@gmail.com>
2022-10-31 00:29:07 -0700

    ddffd

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * file: main.h
+ * auth: ladyvee1
+ * desc: Header file containing prototypes for all functions 
+ * used in 0x07-pointers_arrays_strings
+ */
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_strings(char **s, char *to);
+
+#endif /*MAIN.H*/
