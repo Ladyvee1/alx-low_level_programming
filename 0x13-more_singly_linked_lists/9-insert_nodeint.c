1f46f0b3029d6b9d14d24b8cf934f98093b5c985
ladyvee1 <veekayasuor@gmail.com>
2022-11-30 20:50:42 -0800

    jiuoihio

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node to a listint_t
+ *                           list at a given positon
+ * @head: a pointer to the address of the 
+ *        head of the listint_t list.
+ *
